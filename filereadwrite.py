cfad1d208e402fa3a3cfdc55cef56345facffd09
Chinmay Sahasrabudhe <csahas922@gmail.com>
2025-10-18 21:34:30 -0400

    Add skeleton for i/o code

---
+++ filereadwrite.py
@@ -0,0 +1,7 @@
+# I/O functions for reading and writing data
+
+# Read data from an input file (containing number of atoms, grid dimensions, etc.)
+
+# Write program output to a file (containing final positions of atoms, energies, etc.)
+
+# Append logs to a log file (containing simulation progress, errors, etc.)
